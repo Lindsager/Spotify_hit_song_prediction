a8a6085655f60af0ca2d6712dad5f910d1155d85
Lindsager <lindsager@gmail.com>
2024-03-11 18:01:45 -0700

    updates

--- src/05_model_evaluation.py
+++ src/05_model_evaluation.py
@@ -16,7 +16,6 @@ import seaborn as sns
 from sklearn.model_selection import learning_curve
 from sklearn.metrics import confusion_matrix
 
-
 import warnings
 warnings.simplefilter(action='ignore', category=FutureWarning)
 
@@ -26,6 +25,7 @@ def read_split_data(filepath_xtest, filepath_ytest):
     y_test = pd.read_csv(filepath_ytest).squeeze()
     return X_test_scaled, y_test
 
+#load in the pre-trained models
 def load_pretrained_models(lr_filepath, rf_filepath, knn_filepath, xgb_filepath):
     logistic_regression = joblib.load(lr_filepath)
     random_forest = joblib.load(rf_filepath)
@@ -33,28 +33,15 @@ def load_pretrained_models(lr_filepath, rf_filepath, knn_filepath, xgb_filepath)
     xgboost = joblib.load(xgb_filepath)
     return logistic_regression, random_forest, knn, xgboost
 
+#print the confusion matrix in tabular form
 def plot_confusion_matrix(y_test, predictions, model, labels=['Non-Hit', 'Hit']):
     conf_matrix = confusion_matrix(y_test, predictions)
-    plt.figure(figsize=(5, 4))
-    sns.heatmap(conf_matrix, annot=True, fmt='d', cmap='Blues', xticklabels=labels, yticklabels=labels)
-    plt.title(f'{model} Confusion Matrix')
-    plt.xlabel('Predicted')
-    plt.ylabel('Actual')
-    plt.show()
+    conf_matrix_df = pd.DataFrame(conf_matrix, 
+                         index=['Actual Non-Hit Song', 'Actual Hit Song'], 
+                         columns=['Predicted Non-Hit Song', 'Predicted Hit Song'])
+    return conf_matrix_df
 
-def plot_precision_recall_curve(y_test, predictions, title='Precision-Recall Curve'):
-    precision, recall, thresholds = precision_recall_curve(y_test, predictions)
-    pr_auc = auc(recall, precision)
-    plt.figure(figsize=(5, 3))
-    plt.plot(recall, precision, label=f'PR AUC = {pr_auc:.2f}', linewidth=2)
-    plt.xlabel('Recall', fontsize=14)
-    plt.ylabel('Precision', fontsize=14)
-    plt.title(title, fontsize=16)
-    plt.legend(loc='best')
-    plt.grid(True)
-    plt.tight_layout()
-    plt.show()
-    
+#print the feature importance (or coefficients) for each model
 def print_feature_importance(model, column_names):
     # Logistic regression has coefficients
     if hasattr(model, 'coef_'):
@@ -78,15 +65,13 @@ def print_feature_importance(model, column_names):
     else:
         print(f"{type(model).__name__} does not provide feature importance.")
 
-    
+# format all of the output functions with text    
 def evaluate_model(model, X_test_scaled, y_test, column_names):
     predictions = model.predict(X_test_scaled)
     print("Classification Report:")
     print(classification_report(y_test, predictions))
     print("Confusion Matrix Results:")
     print(plot_confusion_matrix(y_test, predictions, model, labels=['Non-Hit', 'Hit']))
-    print("Precision-Recall Curve:")
-    print(plot_precision_recall_curve(y_test, predictions, title='Precision-Recall Curve'))
     print('Feature Importance Results:')
     print(print_feature_importance(model, column_names))  
 
@@ -94,17 +79,19 @@ def evaluate_model(model, X_test_scaled, y_test, column_names):
 # Main pipeline execution
 def main():
     X_test_scaled, y_test = read_split_data('../data/final/X_test_scaled.csv', 
-                                                                     '../data/final/y_test.csv' )
+                                            '../data/final/y_test.csv' )
     
     logistic_regression, random_forest, knn, xgboost = load_pretrained_models('../models/logistic_regression_model.pkl',
-                                                                                '../models/random_forest_model.pkl', 
-                                                                                '../models/knn_model.pkl',
-                                                                                '../models/xgboost_model.pkl')
+                                                                              '../models/random_forest_model.pkl', 
+                                                                              '../models/knn_model.pkl',
+                                                                              '../models/xgboost_model.pkl')
     column_names = (pd.read_csv('../data/final/X_test_scaled.csv')).columns.tolist()
     models = [logistic_regression, random_forest, knn, xgboost]
     for model in models:
-        print(f"Evaluate {model} model")
-        evaluate_model(model, X_test_scaled, y_test, column_names)
+            print(f"Evaluating {model} model...")
+            evaluate_model(model, X_test_scaled, y_test, column_names)
+
+
 
 if __name__ == '__main__':
     main()
